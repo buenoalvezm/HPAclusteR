704a57c75963718a567dd2a9f07ab279fa2fdcde
María Bueno Álvez <buenoalvezm@gmail.com>
2025-08-18 15:25:39 +0200

    move theme into function

--- R/theme_hpa.R
+++ R/theme_hpa.R
@@ -1,7 +1,22 @@
-theme_simple_2 <-
-  theme(panel.background = element_rect(fill = NA, colour = NA),
-        plot.background = element_rect(fill = NA, color = NA),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        axis.line = element_line(colour="black",size=0.5))
+#' A simple theme for visualizations
+#'
+#' @return A ggplot2 theme object that can be added to plots with `+`.
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(mpg, aes(displ, hwy)) +
+#'   geom_point() +
+#'   theme_cluster()
+#'
+          theme_cluster <-
+            function() {
+              theme(
+                panel.background = element_rect(fill = NA, colour = NA),
+                plot.background = element_rect(fill = NA, color = NA),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.border = element_blank(),
+                axis.line = element_line(colour = "black", size = 0.5)
+              )
+            }
